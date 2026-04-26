5a5f2148716354f7e3904ee1aea62dc5ff729fb3
Zaid <zaidiscoding@gmail.com>
2026-04-26 15:19:41 +0530

    elephant 617A

---
+++ Codeforces/Elephant_617A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Elephant_617A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int steps = n / 5;
+        if (n % 5 != 0) {
+            steps++;
+        }
+        System.out.println(steps);
+    }
+}
