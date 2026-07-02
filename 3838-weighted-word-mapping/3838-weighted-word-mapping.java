e70eda16054fba7673a8d667ae3364633083533d
Mohd Zaid <127447741+Mohd-ZaidCode@users.noreply.github.com>
2026-07-02 15:22:13 +0530

    Time: 6 ms (16.1%), Space: 46.8 MB (11.09%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        String res="";
+        for(String s:words){
+            int sum=0;
+            for(char c:s.toCharArray()){
+                sum+=weights[c-'a'];
+            }
+            res+=(char)('z'-(sum%26));
+        }
+
+        return res;
+    }
+}
