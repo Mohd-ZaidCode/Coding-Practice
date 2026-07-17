12612866e76b6f29297a8d28d8e15813ec761fb3
Mohd Zaid <127447741+Mohd-ZaidCode@users.noreply.github.com>
2026-07-17 15:11:33 +0530

    Time: 2 ms (93.08%), Space: 43.6 MB (28.41%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -1,16 +1,12 @@
 class Solution {
     public int maxNumberOfBalloons(String text) {
-        Map<Character,Integer>map=new HashMap<>();
-        for(char c:text.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
-        int a=Integer.MAX_VALUE;
-        a=Math.min(a,map.getOrDefault('b',0));
-        a=Math.min(a,map.getOrDefault('a',0));
-        a=Math.min(a,map.getOrDefault('l',0)/2);
-        a=Math.min(a,map.getOrDefault('o',0)/2);
-        a=Math.min(a,map.getOrDefault('n',0));
-        return a;
-
+        int[] counts = new int[26];
+        for (char c : text.toCharArray()) counts[c - 'a']++;
+        int b = counts['b' - 'a'];
+        int a = counts['a' - 'a'];
+        int l = counts['l' - 'a'] / 2;
+        int o = counts['o' - 'a'] / 2;
+        int n = counts['n' - 'a'];
+        return Math.min(b, Math.min(a, Math.min(l, Math.min(o, n))));
     }
 }
