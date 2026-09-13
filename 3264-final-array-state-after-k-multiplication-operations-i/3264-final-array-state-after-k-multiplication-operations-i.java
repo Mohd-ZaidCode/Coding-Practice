68a2e3a66b468edfa1831a1f0c92f3ec97f311bc
Mohd Zaid <127447741+Mohd-ZaidCode@users.noreply.github.com>
2026-09-13 17:12:11 +0530

    Time: 1 ms (98.51%), Space: 46.6 MB (77.29%) - LeetHub

---
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        while(k-->0){
+            int idx=0;
+            int max=Integer.MAX_VALUE;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]<max){max=nums[i];idx=i;}
+            }
+            nums[idx]*=multiplier;
+        }
+        return nums;
+    }
+}
