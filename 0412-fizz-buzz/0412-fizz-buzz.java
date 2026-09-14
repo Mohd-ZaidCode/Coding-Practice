8bb305e837cc6658488085b325e1b53e8d3d9ed4
Mohd Zaid <127447741+Mohd-ZaidCode@users.noreply.github.com>
2026-09-14 22:23:52 +0530

    Time: 3 ms (3.13%), Space: 46.9 MB (26.57%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String>ls=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0){
+                if(i%5==0)ls.add("FizzBuzz");
+                else ls.add("Fizz");
+            }
+            else if(i%5==0)ls.add("Buzz");
+            else ls.add(i+"");
+        }
+        return ls;    }
+}
