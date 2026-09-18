c856b1cd9ec1183fd5905f96cbfa6cfeadd1880d
Mohd Zaid <127447741+Mohd-ZaidCode@users.noreply.github.com>
2026-09-18 21:36:14 +0530

    Time: 1 ms (58.71%), Space: 44.3 MB (84.77%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -1,8 +1,8 @@
 class Solution {
     public int maxProduct(int[] nums) {
-    int a=Math.min(nums[0],nums[1]);
-    int b=Math.max(nums[0],nums[1]);
-    for(int i=2;i<nums.length;i++){
+    int a=-1;
+    int b=-1;
+    for(int i=0;i<nums.length;i++){
         if(nums[i]>=b){a=b;b=nums[i];}
         else if(nums[i]>a)a=nums[i];
         
