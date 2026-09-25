73b8102fa080a35ab15d67f41e5a7d2f47d31ef9
Mohd Zaid <127447741+Mohd-ZaidCode@users.noreply.github.com>
2026-09-25 21:48:11 +0530

    Time: 9 ms (20.1%), Space: 43.1 MB (54.8%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -4,10 +4,9 @@ class Solution {
         Map<Character,Integer>map=new HashMap<>();
         for(char c:s.toCharArray()){
             map.put(c,map.getOrDefault(c,0)+1);
+            if(map.get(c)%2==0)res+=2;
         }
-        for(char c:map.keySet()){
-            res+=map.get(c)-map.get(c)%2;
-        }
+        
         if(res!=s.length())return res+1;
         return res;
     }
