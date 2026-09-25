d4c00ed0e6b887f6fe5278516b4f8b1ca44610cb
Mohd Zaid <127447741+Mohd-ZaidCode@users.noreply.github.com>
2026-09-25 21:53:31 +0530

    Time: 2 ms (73.17%), Space: 42.8 MB (95.6%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,10 +1,10 @@
 class Solution {
     public int longestPalindrome(String s) {
         int res=0;
-        Map<Character,Integer>map=new HashMap<>();
+        int []arr=new int[100];
         for(char c:s.toCharArray()){
-            map.put(c,map.getOrDefault(c,0)+1);
-            if(map.get(c)%2==0)res+=2;
+            arr[c-'A']++;
+            if(arr[c-'A']%2==0)res+=2;
         }
         
         if(res!=s.length())return res+1;
